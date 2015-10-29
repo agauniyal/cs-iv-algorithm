4c869c58bb8df0f921e971d8f17eee8dd0f96c5b
r-dea <r_dea@aol.com>
2015-10-28 22:08:06 -0700

    Update cs-iv-algorithm.h

--- include/cs-iv-algorithm.h
+++ include/cs-iv-algorithm.h
@@ -6,7 +6,7 @@
 #include <iomanip>
 
 class sortT {
-  private:
+   private:
     std::vector<std::vector<long long>> asc;
     std::vector<std::vector<long long>> desc;
     std::vector<std::vector<long long>> random;
@@ -26,12 +26,56 @@ class sortT {
         seed();
     };
 
+ public:
+    // default + param constructor -> calls seed()
+    sortT(unsigned int lim = 3) : upLim(lim) {
+        if (upLim < 1)
+            upLim = 3;
+        else {
+            unsigned int s = findSentinel();
+            if (upLim > s) { upLim = 3; }
+        }
+        seed();
+    };
+
     // exposing list in public
     std::vector<std::tuple<std::chrono::microseconds, std::chrono::microseconds,
                            std::chrono::microseconds>> list;
 
     void testWith(void sortF(std::vector<long long>::iterator,
                              std::vector<long long>::iterator));
-    void printList();
+    void sortT::printList() {
+    unsigned int i = 1;
+
+    std::cout << std::left;
+    std::cout << std::setw(10) << "Iteration" << std::setw(0) << "|\t";
+    std::cout << std::setw(10) << "limit" << std::setw(0) << "|\t";
+    std::cout << std::setw(10) << "RandomBox" << std::setw(0) << "|\t";
+    std::cout << std::setw(10) << "AscBox" << std::setw(0) << "|\t";
+    std::cout << std::setw(10) << "DescBox" << std::setw(0) << "|\t";
+
+    for (auto &listTuple : list) {
+
+        unsigned int j = i;
+        long long limit = 1;
+        while (j > 0) {
+            limit *= 10;
+            --j;
+        }
+
+        std::cout << "\n";
+        std::cout << std::setw(10) << i << std::setw(0) << "|\t";
+        std::cout << std::setw(10) << limit << std::setw(0) << "|\t";
+        std::cout << std::setw(10) << std::get<0>(listTuple).count()
+                  << std::setw(0) << "|\t";
+        std::cout << std::setw(10) << std::get<1>(listTuple).count()
+                  << std::setw(0) << "|\t";
+        std::cout << std::setw(10) << std::get<2>(listTuple).count()
+                  << std::setw(0) << "|\t";
+
+        ++i;
+    }
+    std::cout << std::endl;
+}
     unsigned int getUpLim() { return upLim; }
 };
